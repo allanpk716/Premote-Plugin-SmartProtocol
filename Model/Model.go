2126fddd4bdc6a6bff4878ab72cd614c6b533468
allan716 <525223688@qq.com>
2021-03-30 14:22:10 +0800

    Improved robustness
    Signed-off-by: allan716 <525223688@qq.com>

--- Model/Model.go
+++ Model/Model.go
@@ -12,21 +12,18 @@ func timeCost(start time.Time, addInfo *AddressInfo) {
 }
 
 func checkOne(check func(host string, port string) error, sp *SmartProtocol) (string, error) {
-	//wg := &sync.WaitGroup{}
 	var ch = make(chan AddressInfo)
 	defer close(ch)
 	for _, info := range sp.MultiAddressInfo {
 		tmpInfo := info
-		//wg.Add(1)
 
 		go func(info AddressInfo) {
 			nowTime := time.Now()
 			defer func() {
 				timeCost(nowTime, &tmpInfo)
-				//wg.Done()
 				ch <- tmpInfo
 			}()
-
+			// deal timeout in this func check(), so this part don't need deal with it.
 			err := check(tmpInfo.IP, tmpInfo.Port)
 			if err != nil {
 				tmpInfo.Worked = false
@@ -35,9 +32,11 @@ func checkOne(check func(host string, port string) error, sp *SmartProtocol) (st
 			}
 		}(info)
 	}
-	//wg.Wait()
 	for i := 0; i < len(sp.MultiAddressInfo); i++ {
-		nowInfo := <- ch
+		nowInfo, bok := <- ch
+		if bok == false {
+			return "", ErrCheckProtocolError
+		}
 		for index := range sp.MultiAddressInfo {
 			if sp.MultiAddressInfo[index].IP == nowInfo.IP && sp.MultiAddressInfo[index].Port == nowInfo.Port {
 				sp.MultiAddressInfo[index] = nowInfo
